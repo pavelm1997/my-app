60b05f391ec874e084339e4d15acdef84bf6de51
pavelm1997 <34282964+pavelm1997@users.noreply.github.com>
2019-04-01 16:15:18 +0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,6 @@ const Header = () => {
     return <header className='header'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/9/9b/Logo_F%C3%A9d%C3%A9ration_Ukraine_Football_2016.svg' />
     </header>
-};
+}
 
 export default Header;
