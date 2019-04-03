2100a801e4b2dacd44471632c397b4f379ea0a6f
pavelm1997 <34282964+pavelm1997@users.noreply.github.com>
2019-04-03 11:13:38 +0300

    lesson 15

--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import c from './MyPosts.module.css';
+import Post from "./Post/Post";
 let MyPost = () => {
     return ( <div >
         <div className={c.item}>
             my-post
+            <div>
+            <textarea>This write your post....</textarea>
+            <button>Add post</button>
+            </div>
         </div>
-        <div className={c.item}>
-            New post
-        </div>
-        <div className={c.item}>
-            1post
-        </div>
-        <div className={c.item}>
-            2post
-        </div>
-        <div className={c.item}>
-            3post
-        </div>
+      <Post/>
     </div>);};
 export default MyPost;
