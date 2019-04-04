fe3db41bdf6ebcb5681fd6f47d0d5e72aea9b795
Eugen <mashniskuy@gmail.com>
2019-04-04 12:52:43 +0300

    lesson 18

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import c from './App.module.css';
 import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/nav.jsx";
 import Profile from "./components/Profile/Profile.jsx";
+import Dialog from "./components/Dialogs/Dialogs";
+
 const App = () => {
 
     return (
-        <div className = {c.app_wriper}>
-             <Header/>
+        <div className={c.app_wriper}>
+            <Header/>
             <Navbar/>
-            <Profile/>
+            <div className={c.app_wriper_content}>
+
+                <Dialog/>
+            </div>
+            {/*<Profile/>*/}
         </div>
 
     );
