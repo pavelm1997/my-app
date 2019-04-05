baab407d7a3a37654af5a91b1ce14345a0e4e47e
Eugen <mashniskuy@gmail.com>
2019-04-05 13:54:32 +0300

    lesson 24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,32 +8,50 @@ const DialogItem = (props) => {
         <NavLink to={path}> {props.name} </NavLink>
     </div>);
 };
-const Messange =(props) => {
-  return (
-      <div className={c.message}>
-          {props.message}
-      </div>);
+const Messange = (props) => {
+    return (
+        <div className={c.message}>
+            {props.message}
+        </div>);
 };
 
 const Dialog = () => {
+    let dialogData = [
+        {id: 1, name: 'Vasiliy'},
+        {id: 2, name: 'Pavlo'},
+        {id: 3, name: 'Petrovich'},
+        {id: 4, name: 'Eugen'},
+        {id: 5, name: 'I'},
+        {id: 6, name: 'Shura'},
+        {id: 7, name: 'Mikola'},
+    ];
+    let massageData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'I'},
+        {id: 3, message: 'I go to the school'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+        {id: 6, message: 'Yo'},
+        {id: 7, message: 'Yo'},
+    ];
     return (<div className={c.dialogs}>
             <div className={c.dialog_item}>
-                <DialogItem name="Vasiliy" id="1"/>
-                <DialogItem name="Pavlo" id="2"/>
-                <DialogItem name="Petrovich" id="3"/>
-                <DialogItem name="Eudgen" id="4"/>
-                <DialogItem name="I" id="5"/>
-                <DialogItem name="Shura" id="6"/>
-                <DialogItem name="Mikola" id="6"/>
+                <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
+                <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
+                <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
+                <DialogItem name={dialogData[3].name} id={dialogData[3].id}/>
+                <DialogItem name={dialogData[4].name} id={dialogData[4].id}/>
+                <DialogItem name={dialogData[5].name} id={dialogData[5].id}/>
+                <DialogItem name={dialogData[6].name} id={dialogData[6].id}/>
             </div>
             <div className={c.messages}>
-                <Messange message = 'Hi'/>
-                <Messange message = 'I'/>
-                <Messange message = 'I go to the school'/>
-                <Messange message = 'Yo'/>
-                <Messange message = 'Yo'/>
-                <Messange message = 'Yo'/>
-                <Messange message = 'Yo'/>
+                <Messange message={massageData[0].message} id={massageData[0].id}/>
+                <Messange message={massageData[1].message} id={massageData[1].id}/>
+                <Messange message={massageData[2].message} id={massageData[2].id}/>
+                <Messange message={massageData[3].message} id={massageData[3].id}/>
+                <Messange message={massageData[4].message} id={massageData[4].id}/>
+                <Messange message={massageData[5].message} id={massageData[5].id}/>
+                <Messange message={massageData[6].message} id={massageData[6].id}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -3,6 +3,11 @@ import c from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 let MyPost = () => {
+    let postData = [
+        {id: 1, message: 'Hi', like: '45'},
+        {id: 2, message: 'It\'s my first post', like: '19'},
+        {id: 3, message: 'It\'s my second post', like: '7'},
+    ];
     return (<div>
         <div className={c.postBlock}>
             <h3 className={c.post}> my-posts</h3>
@@ -15,9 +20,9 @@ let MyPost = () => {
                 </div>
             </div>
         </div>
-        <Post message="How are you" like="15"/>
-        <Post message="It's my first post" like="19"/>
-        <Post message="It's my second post" like="5"/>
+        <Post message={postData[0].message} like={postData[0].like}/>
+        <Post message={postData[1].message}  like={postData[1].like}/>
+        <Post message={postData[2].message}  like={postData[2].like}/>
     </div>);
 };
 export default MyPost;
