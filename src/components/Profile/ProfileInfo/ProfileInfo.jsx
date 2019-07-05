2ab6642e2ac9bae84cc4ca1ab55b56c7eb18d41c
pavelm1997 <34282964+pavelm1997@users.noreply.github.com>
2019-07-05 16:31:37 +0300

    red

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import c from './ProfileInfo.module.css';
+
 let ProfileInfo = () => {
-    return ( <div className = {c.content}>
-        <img className = {c.img} src = 'https://millionmilesecrets.com/wp-content/uploads/denali-national-park-1733313_1280.jpg'/>
+    return ( <div className={c.content}>
+        <img className={c.img}
+             src='https://millionmilesecrets.com/wp-content/uploads/denali-national-park-1733313_1280.jpg'/>
         <div className={c.item}>
             ava+description
         </div>
-
-    </div>);};
+    </div>);
+};
 export default ProfileInfo;
