32ab87f3bbbb7051a4020c8680f784ca1911ee8e
pavelm1997 <34282964+pavelm1997@users.noreply.github.com>
2019-07-08 22:37:49 +0300

    lessons 28

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 let postData = [
     {id: 1, message: 'Hi', like: '45'},
     {id: 2, message: 'It\'s my first post', like: '19'},
     {id: 3, message: 'It\'s my second post', like: '7'},
 ];
-let dialogDat = [
-    {id: 1, name: 'Vasiliy'},
-    {id: 2, name: 'Pavlo'},
-    {id: 3, name: 'Petrovich'},
-    {id: 4, name: 'Eugen'},
-    {id: 5, name: 'I'},
-    {id: 6, name: 'Shura'},
-    {id: 7, name: 'Mikola'}
-];
+
 let massageData = [
     {id: 1, message: 'Hi'},
     {id: 2, message: 'I'},
@@ -26,11 +19,21 @@ let massageData = [
     {id: 6, message: 'Yo'},
     {id: 7, message: 'Yo'}
 ];
+
+let dialogDat = [
+    {id: 1, name: 'Vasiliy'},
+    {id: 2, name: 'Pavlo'},
+    {id: 3, name: 'Petrovich'},
+    {id: 4, name: 'Eugen'},
+    {id: 5, name: 'I'},
+    {id: 6, name: 'Shura'},
+    {id: 7, name: 'Mikola'}
+];
+
 ReactDOM.render(<App date ={postData} dialog={dialogDat} messege={massageData}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
 
